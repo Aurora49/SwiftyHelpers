b39b7cd56474a61a219ff629aa67550902b1c663
Desmars Sliman <napsters.desmars@gmail.com>
2024-11-30 22:52:43 +0100

    [ADD] Add format extension for strings
    - Add format for email
    - Add format for phone number

--- Sources/SwiftyHelpers/Extensions/Strings/String+isValidEmail.swift
+++ Sources/SwiftyHelpers/Extensions/Strings/String+isValidEmail.swift
@@ -1,6 +1,5 @@
 //
-//  File.swift
-//  
+//  String+isValidEmail.swift
 //
 //  Created by Sliman Desmars on 28/06/2024.
 //
@@ -9,9 +8,15 @@ import Foundation
 
 extension String {
     
-    public func isValidEmail(rule: NSPredicate = NSPredicate(format:"SELF MATCHES %@", "^\\w+[\\w-\\.]*\\@\\w+[\\w-\\.]*\\w+\\.[a-z]{2,3}$")) throws -> Bool {
+    /// Return a `Bool`   to indicate whether or not the string has a valid format for an email.
+    /// - Parameter rule: Predicate used to validate the email format. If this argument is omitted, this function will use a default predicate based on the name@domain.com rule.
+    /// - Returns: True if the email has a valid format.
+    /// - Author: Desmars Sliman
+    /// - Important: If the format is invalid, an exception is thrown.
+    @available(macOS 10.10, iOS 8.0, watchOS 2.0, tvOS 9.0, *)
+    public func isValidEmail(rule: NSPredicate = DefaultValues.Predicates.emailFormat.value) throws -> Bool {
             
-        if rule.evaluate(with: self) == false { throw TextfieldsError.invalidEmailFormat }
+        if rule.evaluate(with: self) == false { throw StringFormatErrors.invalidEmailFormat }
             
         else { return true }
     }
